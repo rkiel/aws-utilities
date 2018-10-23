b3751619781b626f5f6d79d3fc90f1b365ea197b
rkiel <cygnus@pobox.com>
2018-10-23 08:02:48 -0400

    build: added install

---
+++ install/lib/setup/commander.rb
@@ -0,0 +1,65 @@
+module Setup
+
+  class Commander
+
+    attr_accessor :argv
+
+    def initialize (argv)
+      @argv = argv
+    end
+
+    def valid?
+      true
+    end
+
+    def help
+      exit
+    end
+
+    def execute
+      File.open("#{ENV['HOME']}/.bash_profile", "a") do |f|
+        f.puts
+        f.puts '#############################################################'
+        f.puts "# added by ~/GitHub/rkiel/aws-utilities/install/bin/setup"
+        f.puts '#############################################################'
+        f.puts 'export AWS_UTILITIES_BIN="~/GitHub/rkiel/aws-utilities/bin"'
+        f.puts 'export PATH=${AWS_UTILITIES_BIN}:$PATH'
+        f.puts '#############################################################'
+        f.puts
+      end
+
+
+      File.open("#{ENV['HOME']}/.bashrc", "a") do |f|
+        f.puts
+        f.puts '#############################################################'
+        f.puts "# added by ~/GitHub/rkiel/aws-utilities/install/bin/setup"
+        f.puts '#############################################################'
+        f.puts 'source ~/GitHub/rkiel/aws-utilities/dotfiles/bashrc'
+        f.puts '#############################################################'
+        f.puts
+      end
+
+    end
+
+    private
+
+    def run_cmd ( cmd )
+      puts
+      puts cmd
+      success = system cmd
+      unless success
+        error "(see above)"
+      end
+      puts
+    end
+
+    def error ( msg )
+      puts
+      puts "ERROR: #{msg}"
+      puts
+      exit
+    end
+
+  end
+
+end
